2b8a3e42c49f6748bf080d03037d8dab4ceaaa5f
Ryan P. Daly <mathemaphysics@gmail.com>
2023-02-02 00:00:20 +0000

    File headers with identifiers added; need to do this to many more files.

--- ros_ws/src/lidarshooter/src/AffineMesh.cpp
+++ ros_ws/src/lidarshooter/src/AffineMesh.cpp
@@ -1,3 +1,14 @@
+/**
+ * @file AffineMesh.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-02-01
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
 #include "AffineMesh.hpp"
 
 lidarshooter::AffineMesh::Ptr lidarshooter::AffineMesh::create()
